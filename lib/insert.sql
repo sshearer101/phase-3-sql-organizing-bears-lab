83368736e6d0d8a97e1eb841bb9cc97d2e44e8c8
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-07-07 21:54:22 -0600

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'Black', 'Mean', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'Brown', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'Brown', 'Mean', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'Black', 'Nice', true);
